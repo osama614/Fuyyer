07b4a711b6b4fb4db1ba7ef75d83d3bc88ede298
osama614 <os.khaled2020@gmail.com>
2020-09-19 15:23:21 +0200

    add the models file

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+from models import db, Artist, Venue, Show
+
 migrate = Migrate(app, db)
 
 
@@ -41,46 +42,7 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
-class Venue(db.Model):
-    __tablename__ = 'venue'
-    id = db.Column(db.Integer, primary_key=True ,unique=True)
-    name = db.Column(db.String, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120),nullable=False)
-    address = db.Column(db.String(120),nullable=False)
-    phone = db.Column(db.String(120),nullable=False)
-    genres = db.Column(db.ARRAY(db.String), nullable=False)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(500))
-    seeking_description = db.Column(db.String(120), nullable=False)
-    seeking_talent = db.Column(db.String(), default=False)
-    website = db.Column(db.String())
-    # the relationship between Show TAble and Venue TAble is one to many
-    shows_ven = db.relationship('Show', backref='venue')
 
-
-class Artist(db.Model):
-    __tablename__ = 'artist'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120),nullable=False )  
-    phone = db.Column(db.String(120), nullable=False)
-    genres = db.Column(db.ARRAY(db.String), nullable=False)
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String())
-    seeking_venue = db.Column(db.String(), default=False)
-    seeking_description = db.Column(db.String(200))
-    website = db.Column(db.String())
-    # the relationship between Show TAble and Artist TAble is one to many
-    shows_art = db.relationship('Show', backref='artist')
-    
-
-class Show(db.Model):
-  __tablename__='show'
-  venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), primary_key=True)
-  artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), primary_key=True)
-  start_time = db.Column(db.String(), nullable=False)
 
 
 #----------------------------------------------------------------------------#
